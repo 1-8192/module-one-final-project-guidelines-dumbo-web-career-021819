f9f35f46f0032e7d7154b370cfdcd0fe55074c38
1-8192 <allegranzia@gmail.com>
2019-03-04 11:42:36 -0500

    Added migrate folder

--- db/migrate/001_create_character_class_table.rb
+++ db/migrate/001_create_character_class_table.rb
@@ -1,4 +1,4 @@
-class CreateCharacterClassTable < ActiveRecord::Migration[5.1]
+class CreateCharacterClassTable < ActiveRecord::Migration[5.0]
   def change
     create_table :character_classes do |t|
       t. string :class_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "character_classes", force: :cascade do |t|
+    t.string  "class_name"
+    t.string  "description"
+    t.integer "spellcasting"
+    t.integer "spell_slots_at_1"
+  end
 
 end
